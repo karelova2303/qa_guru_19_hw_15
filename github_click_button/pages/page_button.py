19710e27dd318647aa6a2e5e81c86fd3901f6bc2
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-18 20:50:29 +0300

    add page_button.py

---
+++ github_click_button/pages/page_button.py
@@ -0,0 +1,20 @@
+from selene import browser, have
+
+
+class PageButton():
+    def __init__(self):
+        self.button_sign_up = browser.element('.HeaderMenu-link--sign-up')
+        self.button_content = browser.element('.Button-content')
+        self.auth_form_header = browser.element(' #signup-form-fields')
+
+    def open_main_page(self):
+        browser.open('https://github.com')
+
+    def should_be_clickable_button_desktop(self):
+        self.button_sign_up.click()
+        self.auth_form_header.should(have.exact_text('Sign up to GitHub'))
+
+    def should_be_clickable_button_mobile(self):
+        self.button_content.click()
+        self.button_sign_up.click()
+        self.auth_form_header.should(have.exact_text('Sign up to GitHub'))
