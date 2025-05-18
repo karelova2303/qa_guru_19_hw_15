1ec994ee486bb26c6bd1c01098005cab84a34877
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-18 20:52:17 +0300

    add test_skip.py.py

---
+++ tests/test_skip.py
@@ -0,0 +1,26 @@
+"""
+Параметризуйте фикстуру несколькими вариантами размеров окна
+Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
+"""
+import pytest
+
+from github_click_button.pages.page_button import PageButton
+
+page_button = PageButton()
+
+
+def test_github_desktop(open_browser):
+    if open_browser == 'mobile':
+        pytest.skip(reason='Pазрешение экрана для мобильных тестов')
+    page_button.open_main_page()
+    page_button.should_be_clickable_button_desktop()
+
+def test_github_mobile(open_browser):
+    if open_browser == 'desktop':
+        pytest.skip(reason='Pазрешение экрана для десктопных тестов')
+    page_button.open_main_page()
+    page_button.should_be_clickable_button_mobile()
+
+
+
+
