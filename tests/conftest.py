74884e1071e25f0a0c88a4dc249d06bbf1b57ef6
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-18 20:49:37 +0300

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,55 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture()
+def mobile_setup_browser():
+    browser.config.window_width = '400'
+    browser.config.window_height = '634'
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture()
+def desktop_setup_browser():
+    browser.config.window_width = '1920'
+    browser.config.window_height = '1080'
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(params=['mobile', 'desktop'])
+def setup_browser(request):
+    if request.param == 'mobile':
+        browser.config.window_width = '400'
+        browser.config.window_height = '634'
+
+    if request.param == 'desktop':
+        browser.config.window_width = '1920'
+        browser.config.window_height = '1080'
+
+    yield
+
+    browser.quit()
+
+
+desktop_only = pytest.mark.parametrize('setup_browser', ['desktop'], indirect=True)
+mobile_only = pytest.mark.parametrize('setup_browser', ['mobile'], indirect=True)
+
+
+@pytest.fixture(params=[(400, 634), (360, 740), (1920, 1080), (1366, 768)])
+def open_browser(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    if width < height:
+        yield 'mobile'
+    else:
+        yield 'desktop'
+
+
+    browser.quit()
